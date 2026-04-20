2c28b179375be0ebfe8ce8cb7abb7b9e1641567d
unknown <harshshinde200328@gmail.com>
2026-04-20 19:55:40 +0100

    Fix syntax error in app.py

--- app.py
+++ app.py
@@ -2,31 +2,28 @@ from flask import Flask, render_template, request, send_from_directory
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
-
 
 @app.route("/resume")
 def resume():
     return render_template("resume.html")
 
 
-    )
 @app.route("/download-resume")
 def download_resume():
     return send_from_directory(
         directory="static/files",
-        path="Harsh_Shinde_Resume_V2.pdf",
+        path="Harsh_Shinde_Resume_v2.pdf",
         as_attachment=True,
         download_name="Harsh_Shinde_Resume.pdf"
     )
+
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
